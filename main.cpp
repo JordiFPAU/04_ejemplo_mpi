f16b2aad57a22db019638105e56dba63b1c7ca63
JordiFPAU <jfpila@uce.edu.ec>
2026-01-13 16:33:41 -0500

    Primer Commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fmt/core.h>
+#include <mpi.h>
+int main(int args, char** argv)
+{
+    MPI_Init(&args, &argv);
+    int nprocs;
+    int rank;
+
+    //- ranks
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs); // numero de procesos
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);   // id del proceso
+    //- version 
+    int version, subversion;
+    MPI_Get_version(&version, &subversion);
+
+    if(rank == 0){
+        fmt::print("Version MPI: {}.{}\n", version, subversion);
+        fmt::print("Numero de procesos: {}\n", nprocs);
+    }
+    
+    fmt::println("Rank_{} de {} procesos", rank, nprocs);
+    std::cout.flush();
+    //while(1<2){
+    //}
+
+
+    MPI_Finalize();
+    return 0;
+}
